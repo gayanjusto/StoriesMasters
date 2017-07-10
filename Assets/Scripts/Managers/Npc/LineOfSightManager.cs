d44f67c9252607165ddd01c35e6efeb0cc0ff023
gayanjusto <gayan.justo@gmail.com>
2017-07-10 15:48:39 -0300

    . Criação LineOfSightManager . Inserção RigidBody Player & Enemy

---
+++ Assets/Scripts/Managers/Npc/LineOfSightManager.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts.Constants;
+using UnityEngine;
+
+namespace Assets.Scripts.Managers.Npc
+{
+    public class LineOfSightManager : BaseMonoBehaviour
+    {
+        void OnTriggerEnter(Collider other)
+        {
+            if(other.tag == Tags.PlayerTag)
+            {
+                Debug.Log(transform.parent.name +  " achou player em Line of Sight");
+            }
+        }
+    }
+}
